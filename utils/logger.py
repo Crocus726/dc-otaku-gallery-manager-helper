c330a10c5abb92370e22f2870c52582dfdd520a2
Crocus726 <crocus726@gmail.com>
2026-03-17 11:29:14 +0900

    Add TimeLogger class and setup_logger function
    Implement TimeLogger class for logging with timestamps and create setup_logger function to initialize logging.

---
+++ utils/logger.py
@@ -0,0 +1,34 @@
+import sys
+import os
+from datetime import datetime
+
+class TimeLogger:
+    def __init__(self, filename):
+        self.terminal = sys.stdout
+        self.log = open(filename, "a", encoding = "utf-8")
+        self.is_new_line = True
+    def write(self, message):
+        if message == '\n':
+            self.terminal.write(message)
+            self.log.write(message)
+            self.log.flush()
+            self.is_new_line = True
+            return
+        if self.is_new_line:
+            now = datetime.now().strftime("[%Y-%m-%d %H:%M:%S] ")
+            self.terminal.write(now)
+            self.log.write(now)
+            self.is_new_line = False
+        self.terminal.write(message)
+        self.log.write(message)
+        self.log.flush()
+    def flush(self):
+        pass
+
+def setup_logger():
+    if not os.path.exists("logs"):
+        os.makedirs("logs")
+    log_start_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    log_filename = f"logs/log_{log_start_time}.txt"
+    sys.stdout = TimeLogger(log_filename)
+    return log_filename
